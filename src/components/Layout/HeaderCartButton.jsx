d5993379ef374016c5b5f6af8aeb444a71e476e6
neo-kjb <anaszawahreh.dev@gmail.com>
2023-01-29 16:44:12 +0300

    Bumping the Cart Icon

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,16 +1,31 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import styles from "./HeaderCartButton.module.css";
 import CartContext from "../../store/cart-context";
 
 export default function HeaderCartButton({ onClick }) {
+  const [btnHighlihgted, setBtnHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
   const numberOfCartItems = cartCtx.items.reduce((curVal, item) => {
     return curVal + item.amount;
   }, 0);
+  const btnClasses = `${styles.button} ${btnHighlihgted ? styles.bump : ""}`;
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartCtx.items]);
 
   return (
-    <button className={styles.button} onClick={onClick}>
+    <button className={btnClasses} onClick={onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
